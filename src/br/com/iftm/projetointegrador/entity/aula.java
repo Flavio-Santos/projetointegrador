7748c788d2c321f8a8b64d486748b7e2e740379d
Ronney Petras <ronneypetras@gmail.com>
2017-07-02 18:44:09 -0300

    Voluntarios e Eventos
    Regras de associacao entre voluntarios e eventos. Feito apos reuniao na
    casa do Ronney

--- src/br/com/iftm/projetointegrador/entity/aula.java
+++ src/br/com/iftm/projetointegrador/entity/aula.java
@@ -1,6 +1,9 @@
 package br.com.iftm.projetointegrador.entity;
 
+import java.sql.SQLException;
 import java.util.Date;
+
+import br.com.iftm.projetointegrador.dao.VoluntarioDAO;
 
 public class aula {
 	public static void main(String[] args) {
@@ -11,7 +14,17 @@ public class aula {
 		Evento e1 = new Evento("Super Doacao","Evento de teste", hoje, hoje, c,v);
 		Evento e2 = new Evento("Mega Doacao","Evento de teste", hoje, hoje, c,v);
 		
-		System.out.println(v);
+		
+		VoluntarioDAO vd = new VoluntarioDAO();
+		try {
+			vd.getVoluntario(1);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
